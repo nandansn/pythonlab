b3074891e4aff39995bd4e6fe6275f3d92a81da3
Nandakumar Rangasamy <Nandakumar_Rangasamy@epam.com>
2017-07-06 12:30:45 +0530

    updated prj

--- learnings/List/iterateList.py
+++ learnings/List/iterateList.py
@@ -2,3 +2,8 @@ listOfNames =['nanda','sanmathi','nivrithi']
 
 for name in listOfNames:
     print(name);
+
+
+print(listOfNames[2])
+
+#print(listOfNames[10]) IndexError: list index out of range
---
+++ learnings/Operators/arithmetic.py
@@ -0,0 +1,13 @@
+number = 1 + 2+ 3 / 2;
+
+print(number);
+
+reminder = 11 % 2;
+
+print(reminder)
+
+squared = 2 ** 2
+print(squared)
+
+cubed = 2 ** 3
+print(cubed)

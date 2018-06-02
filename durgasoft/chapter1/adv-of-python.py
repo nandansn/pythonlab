7c415ea4e3af2df0946f08712f3251d9a03e3060
Nandakumar R <geeknanda13@gmail.com>
2018-06-02 19:53:03 +0530

    updated

---
+++ durgasoft/chapter1/adv-of-python.py
@@ -0,0 +1,2 @@
+print('Advantages of python')
+print('1. easy to learn \n 2. platform independent \n 3. portablility \n 4. dynamically typed language \n 5. Open source \n 6. High level programming language')

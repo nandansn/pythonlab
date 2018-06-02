3aaa300c7183e1c058a29baec2a1fbfecdcb40e5
Nandakumar R <geeknanda13@gmail.com>
2018-06-02 19:40:03 +0530

    added new file

---
+++ durgasoft/chapter1/pgm1.py
@@ -0,0 +1,9 @@
+print('i have started learning python....\n')
+print('thank god for giving me this wonderful life...\n')
+print('pyhton is dynamically typed language. here is the example \n')
+a,b =10,20
+print('sum of a nd b is:', a+b)
+print('data type of a and b is:',type(a),type(b))
+print('now assigning the chars to a')
+a='a is one of the alphabhets'
+print('after assigning the chars, now the type of a is :',type(a))
